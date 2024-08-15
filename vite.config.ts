d448d299c987a12a330f07d9ed8b3ba5286a6408
ogo-js <taichi.ogo@japan-systems.co.jp>
2024-08-15 10:56:06 +0900

    feat: add remix-flat-routes

--- vite.config.ts
+++ vite.config.ts
@@ -1,4 +1,5 @@
 import { vitePlugin as remix } from "@remix-run/dev";
+import { flatRoutes } from "remix-flat-routes";
 import { defineConfig } from "vite";
 import tsconfigPaths from "vite-tsconfig-paths";
 
@@ -10,6 +11,8 @@ export default defineConfig({
         v3_relativeSplatPath: true,
         v3_throwAbortReason: true,
       },
+      ignoredRouteFiles: ["**/*"],
+      routes: async (defineRoutes) => flatRoutes("routes", defineRoutes),
     }),
     tsconfigPaths(),
   ],
